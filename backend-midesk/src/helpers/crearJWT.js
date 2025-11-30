20fbca93ade4d8535805f16162d4e71ba8ef756a
xOscar <123287270+Oscar-byte-c@users.noreply.github.com>
2025-11-30 15:32:21 -0500

    Delete backend-midesk directory

--- backend-midesk/src/helpers/crearJWT.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const generarJWT = (id, rol) => {
-    return jwt.sign({ id, rol }, process.env.JWT_SECRET || "palabrasecreta", {
-        expiresIn: '1d' // El token dura 1 día
-    });
-}
-
-export default generarJWT;
